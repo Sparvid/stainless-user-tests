f7aefc308a021a5df73ae7e70faf5230841a314e
Sparvid <arvid@loren.se>
2026-03-11 13:28:45 +0100

    actually using floats

--- src/main/scala/Converge.scala
+++ src/main/scala/Converge.scala
@@ -1,24 +1,20 @@
 import stainless.annotation.* 
 import stainless.lang.*
-import stainless.math.*
 
 @extern
 def main(args: Array[String]): Unit = {
         println(converge())
 }
 
-def converge(): Rational = {
-  var a: Rational = Rational(0)
-  var d: Rational = Rational(1)
+def converge(): Float = {
+  var a: Float = 0
+  var d: Float = 1
   decreases(d)
-
-  while (d > Rational(1,100)) {
-    a = a + d/Rational(2)
-    d = Rational(1) - a
-  }.invariant(
-    a + d == Rational(1) &&
-    a >= Rational(0) &&
-    d >= Rational(0)
-  )
+  while
+      d > 1/100
+  do
+      a = a + d/2
+      d = 1 - a
+  .invariant(a + d == 1 && a >= 0 && d >= 0)
   a
-}.ensuring(res => Rational(99,100) <= res && res <= Rational(1))
+}.ensuring(res => 99 / 100 <= res && res <= 1)
