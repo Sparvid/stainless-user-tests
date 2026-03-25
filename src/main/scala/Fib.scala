4f73dfbe2c8dc76960985654deb54cc0adf2b651
Sparvid <arvid@loren.se>
2026-03-25 16:38:20 +0100

    smol prep

--- src/main/scala/Fib.scala
+++ src/main/scala/Fib.scala
@@ -7,7 +7,7 @@ def main(args: Array[String]): Unit = {
 }
 // 0,1,1,2,3,5,8,13,21,34,55
 
-// TODO: remove verification code, write specification (or not)
+// One correct solution using BigInt
 def fib(n: BigInt): BigInt = {
         require(n >= 0)
         decreases(n)
@@ -15,3 +15,19 @@ def fib(n: BigInt): BigInt = {
         else if n == 1 then BigInt(1)
         else fib(n-2) + fib(n-1)
 }.ensuring(n >= 0)
+
+// Another correct solution?? LEADS TO TIMEOUT, WHY?
+def fib2(n: Int): Int = {
+        require(n >= 0 && n < Int.MaxValue)
+        decreases(n)
+        if n == 0 then 0
+        else if n == 1 then 1
+        else fib2(n-2) + fib2(n-1)
+}.ensuring(n >= 0)
+
+// Task skeleton code
+def fib3(n: Int): Int = {
+        if n == 0 then 0
+        else if n == 1 then 1
+        else fib3(n-2) + fib3(n-1)
+}.ensuring(n >= 0) // Task 1: prove the postcondition
---
+++ src/main/scala/List.scala
@@ -0,0 +1,3 @@
+// (l2 ++ l1).reverse == l1.reverse ++ l2.reverse
+// User writes a proof
+
